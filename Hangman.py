1953305bc605acfe08677996af770c8a3503eb22
Amr Abedo <codemouraa@gmail.com>
2020-11-18 18:08:39 -0500

    removed personal comments

--- Hangman.py
+++ Hangman.py
@@ -4,19 +4,19 @@ import random
 
 #--------------------------------------------------------
 #basic setup
-pygame.init() #ALWAYS initialize pygame to use it (basically setting it up)
+pygame.init() 
 WIDTH, HEIGHT = 800, 550
-window = pygame.display.set_mode((WIDTH,HEIGHT)) #setting up the game window (pygame only accepts a tuple for set_mode so you have to add the extra pair of paranthesis
-pygame.display.set_caption("Hangman") #setting up the label on top of the game
+window = pygame.display.set_mode((WIDTH,HEIGHT)) 
+pygame.display.set_caption("Hangman") 
 FPS = 60
-clock = pygame.time.Clock() #makes sure the loop runs at a specific speed
+clock = pygame.time.Clock()
 
 #--------------------------------------------------------
 #Loading Images
 images = [] 
-for i in range(1,8): #range(inclusive, exlusive) --> check comment on line 15 to see why we chose (1,8) 
-	image = pygame.image.load("hangman" + str(i) + ".png") #this is how you load an image from pygame (it only accepts the name of the image)
-	images.append(image) #add the image to the list
+for i in range(1,8): 
+	image = pygame.image.load("hangman" + str(i) + ".png") 
+	images.append(image) 
 
 #--------------------------------------------------------
 #Game Variables:
@@ -28,11 +28,10 @@ hangman_status = 0
 RADIUS = 20
 GAP = 55
 THICK = 3
-WHITE = (255,255,255) #these need to be in paranthesis because drawing functions only accepts tuples
+WHITE = (255,255,255) 
 BLACK = (0,0,0)
 
-words = [] #has to be all uppercase bc of line 45 when we use chr(A + i), we are starting off at Capital A and ending at Capital Z and
-#were compare if those capital letters in the list match the letters in the word. They will never match if its capital a and lowercase a
+words = []
 with open("words.txt") as f:
 	for i in f:
 		words.append(i.upper().rstrip())
@@ -52,26 +51,25 @@ for i in range(26):
 		buttons.append([x,y,chr(A + i),True]) #char(A+i) will add the number representation of each capital letter A-Z 
 		x += GAP
 
-button_font = pygame.font.SysFont('comicsans', 40) #PARAMETERS ---> font name, font size
+button_font = pygame.font.SysFont('comicsans', 40) 
 word_font = pygame.font.SysFont('comicsans', 60)
 
 #--------------------------------------------------------
 #METHODS
 def display_message(message):
-	window.fill(WHITE) #overrides the entire screen with white
-	text = button_font.render(message, 1, BLACK) #text needs to be rendered first --> Paramers: what you want to render, 1, color of text
+	window.fill(WHITE) 
+	text = button_font.render(message, 1, BLACK)
 	window.blit(text, (350, 250))	
-	pygame.display.update() #you need to update the display everytime if you change anything in the display
-	pygame.time.delay(2000) #delays for 2 seconds
+	pygame.display.update() 
+	pygame.time.delay(2000) 
 
 #--------------------------------------------------------
 #Game Loop 
-#whenever you create a pygame program, you need a loop to constantly check for collisions/ see if you clicked something, see if time runs out, etc.
 run = True
 while run:
-	clock.tick(FPS) #necessary to make sure the while loops runs at the speed we set up on line 9-10
-	window.fill(WHITE) #fills the window with a specific color --> only takes RGB values within a tuple (the two extra paranthesis)
-	window.blit(images[hangman_status], (50,110)) #blit stands for draw (PARAMETERS: (what you want to draw, x,y position of where you want to draw it)
+	clock.tick(FPS) 
+	window.fill(WHITE) 
+	window.blit(images[hangman_status], (50,110)) 
 
 	#Drawing the word
 	display_word = ""
@@ -93,31 +91,29 @@ while run:
 
 	#Drawing the buttons
 	for button in buttons:
-		x,y,letter,visible = button #since the list is in triples, EX(50,70,'B',TRUE) --> x = 50 and y = 70, letter = B, visible = TRUE
+		x,y,letter,visible = button 
 		if visible:
-			pygame.draw.circle(window, BLACK, (x,y), RADIUS, THICK) #PARAMTERS (where you want to draw it, color, where its drawn (the center of the button), radius, witdth of the button)
-			text = button_font.render(letter, 1, BLACK) #text needs to be rendered first --> Paramers: what you want to render, 1, color of text
-			window.blit(text, (x - text.get_width()/2 ,y - text.get_height()/2)) #cant use second paramter as (x,y) because it will start to draw the letter from the upper left corner of the text AT x,y
-																																					 #using text.get_width()/2 and text.get_height/2 will return where the middle of that specific letter is because each letter has a different midpoint		
-	pygame.display.update() #you need to update the display everytime if you change anything in the display
+			pygame.draw.circle(window, BLACK, (x,y), RADIUS, THICK) 
+			text = button_font.render(letter, 1, BLACK) 
+			window.blit(text, (x - text.get_width()/2 ,y - text.get_height()/2))
 
-	#necessary to check for events (clicking a keyboard/mouse etc..)
-	for event in pygame.event.get(): #any event that happens will be stored here
-		if event.type == pygame.QUIT: #if the user clicks the close button
+	pygame.display.update() 
+
+	#check for events
+	for event in pygame.event.get(): 
+		if event.type == pygame.QUIT: 
 			run = false
-		if event.type == pygame.MOUSEBUTTONDOWN: #if the user clicks the mouse
-			m_x, m_y = pygame.mouse.get_pos() #returns the (x,y) position of the mouse 
-			for button in buttons: #loop to every button to see what button was clicked
+		if event.type == pygame.MOUSEBUTTONDOWN: 
+			m_x, m_y = pygame.mouse.get_pos() 
+			for button in buttons: 
 				x,y,letter,visible = button
 				if visible:
-					distance = math.sqrt((m_x - x)**2 + (m_y - y)**2) #(x,y) refer to the center of each specific circle/letter
-																														#distance formula is used to see how far the click was from the center of that circle/letter
-					if distance < RADIUS: #if the distance is less than the radius, that means button click was on that circle
+					distance = math.sqrt((m_x - x)**2 + (m_y - y)**2) 
+					if distance < RADIUS: 
 						button[3] = False
 						guessed.append(button[2])
 						if letter not in word:
 							hangman_status += 1
-						# print(letter) #prints that specific letter
 						break
 
 	pygame.display.update()
@@ -160,14 +156,4 @@ while run:
 			break
 			
 
-pygame.quit() #closes the window 
-
-
-"""
-UNDERSTANDING THE COORDINATE SYSTEM IN PYGAME:
-Unlike the coordinate system in math where the center is (0,0), the top left corner of pygame is (0,0).
-This means that as you move right, the x value increases and when you move left the x value decreases BUT
-as you move down the y value increases and as you move up the y value decreases.
-
-When you draw items in pygame, you start drawing it from the top left corner.
-"""
+pygame.quit() 
